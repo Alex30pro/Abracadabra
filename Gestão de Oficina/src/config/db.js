da63661853f1cc549d7f54ff7dd9eda6cfc40a01
Alex30pro <alexandryopg87@gmail.com>
2024-10-14 17:31:49 -0300

    atualizado

--- Gestão de Oficina/src/config/db.js
+++ Gestão de Oficina/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     mongoose
-        .connect('mongodb://localhost:27017/manutenção') 
+        .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${CLUSTER_ADDRESS}.mongodb.net/manutenção`) 
         .then(() => console.log('Connected to MongoDB')) 
         .catch((error) => console.log(error)); 
 };
